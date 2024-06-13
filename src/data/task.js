c248ef223051b4132f6574831662a71f008684de
JonathanDevW <jona.edu.lopez06@gmail.com>
2024-06-12 20:52:07 -0600

    TaskManager

---
+++ src/data/task.js
@@ -0,0 +1,24 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "Mi primer tareas",
+  },
+  {
+    id: 1,
+    title: "mi segundo tarea",
+    description: "Mi primer tareas",
+  },
+  {
+    id: 2,
+    title: "mi tercerta tarea",
+    description: "Mi tercer tareas",
+  },
+  {
+    id: 3,
+    title: "mi cuarta tarea",
+    description: "Mi cuarta tareas",
+  },
+];
+
+
